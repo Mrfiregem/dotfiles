151b1dfe087eeeaed19161df9e12d17edbb768da
Firegem <mrfiregem@protonmail.ch>
2021-10-28 22:03:09 -0400

    yambar: modify weather module

--- scripts/shell/executable_wttr.sh
+++ scripts/shell/executable_wttr.sh
@@ -1,3 +1,51 @@
-#!/bin/sh
+#!/usr/bin/env bash
+mapfile -t -n 2 out < <(curl -s 'wttr.in/?m&format=%x\n%t\n')
 
-printf 'weather|string|%s\n\n' "$(curl -s 'wttr.in/?m&format=%c+%t')"
+declare icon
+case "${out[0]}" in
+  'o')
+    icon='滛'
+    ;;
+  'm')
+    icon='杖'
+    ;;
+  'mm')
+    icon='摒'
+    ;;
+  'mmm')
+    icon=''
+    ;;
+  '=')
+    icon='敖'
+    ;;
+  '/' | '.')
+    icon='殺'
+    ;;
+  '//' | '///')
+    icon='歹'
+    ;;
+  '*' | '*/')
+    icon='流'
+    ;;
+  '**' | '*/*')
+    icon=''
+    ;;
+  'x' | 'x/')
+    icon='晴'
+    ;;
+  '!/')
+    icon=''
+    ;;
+  '*!*')
+    icon=''
+    ;;
+  '/!/')
+    icon=''
+    ;;
+  *)
+    icon=''
+    ;;
+esac
+
+printf 'icon|string|%s\n' "$icon"
+printf 'weather|string|%s\n\n' "${out[1]}"
