03da802bf8203d6f3038c879e0b5c406fdac44db
Firegem <mrfiregem@protonmail.ch>
2021-11-09 16:57:58 -0500

    yambar: change order of printf in pacman status

--- scripts/shell/executable_pacman.fish
+++ scripts/shell/executable_pacman.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 set pkgs (pacman -Q | wc -l)
-set upds (checkupdates | wc -l)
-
 printf 'pkgcount|int|%d\n'   $pkgs
+
+set upds (checkupdates | wc -l)
 printf 'updcount|int|%d\n\n' $upds
