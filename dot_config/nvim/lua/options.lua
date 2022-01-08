694a6eecd729d06dbf69bf0e45d9d38bb22aeabc
Firegem <mrfiregem@protonmail.ch>
2022-01-08 02:41:44 -0500

    nvim: set global `scrolloff`

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,20 +1,29 @@
-vim.opt.completeopt = {"menu", "menuone", "noselect"}
-
+-- [Options]
+-- Better search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.mouse = "a"
 vim.opt.title = true
+
+-- Allow switching from unsaved buffers
 vim.opt.hidden = true
 
+-- Open split windows differently
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+-- Show line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Buffer between mouse and edge when scrolling.
+vim.opt.scrolloff = 10
+
+-- Gutter to the left of linenumber
 vim.opt.signcolumn = "yes"
 
+-- Show whitespace characters
 vim.opt.list = true
 vim.opt.listchars = {
   tab = "» ",
@@ -24,6 +33,7 @@ vim.opt.listchars = {
   trail = "·",
 }
 
+-- Use 2 spaces for indentation
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 
@@ -33,4 +43,8 @@ vim.opt.termguicolors = true
 vim.api.nvim_command("colorscheme kanagawa")
 require("lualine").setup {options = {theme = "kanagawa"}}
 
+-- vimtex
 vim.g.vimtex_compiler_latexmk_engines = {_ = "-xelatex"}
+
+-- nvim-cmp
+vim.opt.completeopt = {"menu", "menuone", "noselect"}
