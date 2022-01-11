14aa01eba63e422f7d93972e0106b854fff44a3a
Firegem <mrfiregem@protonmail.ch>
2022-01-11 17:50:46 -0500

    nvim: cmp: modify keybinds

--- dot_config/nvim/lua/completion.lua
+++ dot_config/nvim/lua/completion.lua
@@ -1,4 +1,12 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and
+           vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col)
+             :match("%s") == nil
+end
+
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup {
   snippet = {
@@ -7,13 +15,29 @@ cmp.setup {
     end,
   },
   mapping = {
-    ["<kUp>"] = cmp.mapping.scroll_docs(-4),
-    ["<kDown>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({select = true}),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
+    ["<C-e>"] = cmp.mapping({i = cmp.mapping.abort(), c = cmp.mapping.close()}),
+    ["<CR>"] = cmp.mapping.confirm({select = false}),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
   },
-  sources = {
+  sources = cmp.config.sources {
     {name = "nvim_lua"}, -- For neovim's Lua API
     {name = "nvim_lsp"}, -- For LSP completions
     {name = "luasnip"}, -- For snippets
