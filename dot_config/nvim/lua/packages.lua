b1ae5d6ade74a29703bcd9e0b0eb38e3dc3b6f9a
Firegem <mrfiregem@protonmail.ch>
2022-02-02 00:35:15 -0500

    nvim: install null-ls plugin

--- dot_config/nvim/lua/packages.lua
+++ dot_config/nvim/lua/packages.lua
@@ -44,6 +44,13 @@ return require("packer").startup(function(use)
 
   use {"neovim/nvim-lspconfig", opt = true}
   use {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("null-ls").setup()
+    end,
+    requires = {"nvim-lua/plenary.nvim"},
+  }
+  use {
     "hrsh7th/nvim-cmp",
     requires = {
       "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lsp",
