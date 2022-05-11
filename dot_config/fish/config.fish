10f2b1780d5184dad67f15840fef66c01b949a03
Firegem <mrfiregem@protonmail.ch>
2022-05-11 19:49:28 -0400

    fish: modify login commands

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,27 +1,25 @@
-if status is-login
-    if not set -q VISUAL
-        set -gx VISUAL nvim
-        set -gx EDITOR $VISUAL
-    end
+set -x VISUAL nvim
+set -x EDITOR $VISUAL
+eval (ssh-agent -c)
+
+if status is-interactive
+    source ~/.config/fish/colorschemes/kanagawa.fish
+
+    # Abbreviations
+    #-- Editor
+    abbr -a -g v $VISUAL
+
+    #-- Other Programs
+    abbr -a -g ls exa
+    abbr -a -g ch chezmoi
+    abbr -a -g lz lazygit
+    abbr -a -g gd gallery-dl
+
+    #-- Trash-CLI
+    abbr -a -g gp garbage put
+    abbr -a -g gl garbage list
+    abbr -a -g ge garbage empty
+    abbr -a -g gr garbage restore
 end
-
-source ~/.config/fish/colorschemes/kanagawa.fish
-
-# Abbreviations
-#-- Editor
-
-abbr -a -g v $VISUAL
-
-#-- Other Programs
-abbr -a -g ls exa
-abbr -a -g ch chezmoi
-abbr -a -g lz lazygit
-abbr -a -g gd gallery-dl
-
-#-- Trash-CLI
-abbr -a -g gp garbage put
-abbr -a -g gl garbage list
-abbr -a -g ge garbage empty
-abbr -a -g gr garbage restore
 
 # vim: ft=fish
