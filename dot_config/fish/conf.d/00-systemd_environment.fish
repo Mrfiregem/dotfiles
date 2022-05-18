9e24385f35c8e8739f0071e68cb4cba239871ab8
Firegem <mrfiregem@protonmail.ch>
2022-05-17 21:24:28 -0400

    fish: run env generator only on login

--- dot_config/fish/conf.d/00-systemd_environment.fish
+++ dot_config/fish/conf.d/00-systemd_environment.fish
@@ -1,7 +1,8 @@
-#!/usr/bin/env fish
-set -l vars (/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator |
-  sed 's/^\(.*\)=\(.*\)$/set -gx \1 \2/')
+if status is-login
+    set -l vars (/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator |
+      sed 's/^\(.*\)=\(.*\)$/set -gx \1 \2/')
 
-for var in $vars
-  eval $var
+    for var in $vars
+        eval $var
+    end
 end
