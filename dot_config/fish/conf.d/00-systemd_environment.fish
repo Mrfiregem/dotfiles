60c1da914228a30882db4aead2da94e2edfc4d63
Firegem <mrfiregem@protonmail.ch>
2022-05-17 22:04:27 -0400

    only use fish as an interactive shell

--- dot_config/fish/conf.d/00-systemd_environment.fish
+++
@@ -1,8 +0,0 @@
-if status is-login
-    set -l vars (/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator |
-      sed 's/^\(.*\)=\(.*\)$/set -gx \1 \2/')
-
-    for var in $vars
-        eval $var
-    end
-end
--- dot_config/fish/conf.d/10-gpg_agent_ssh.fish
+++
@@ -1,4 +0,0 @@
-if status is-login
-    set -e SSH_AGENT_PID
-    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
-end
