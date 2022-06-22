7fc0bcd1f9801e35a2f2e83718a3714886f41303
Firegem <mrfiregem@protonmail.ch>
2022-06-22 18:33:54 -0400

    fish: add flatpak abbreviations

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,22 +2,21 @@ if status is-interactive
     source ~/.config/fish/colorschemes/kanagawa.fish
 
     # Abbreviations
-    #-- Editor
     abbr -a -g v $VISUAL
-    abbr -a -g ve $EDITOR
 
-    #-- Other Programs
+    abbr -a -g fpi 'flatpak install'
+    abbr -a -g fpu flatpak-uninstall
+
+    abbr -a -g tp trash-put
+    abbr -a -g tl trash-list
+    abbr -a -g te trash-empty
+    abbr -a -g tt trash-restore
+
     abbr -a -g ls exa
     abbr -a -g ch chezmoi
     abbr -a -g lz lazygit
     abbr -a -g gd gallery-dl
     abbr -a -g tg topgrade
-
-    #-- Trash-CLI
-    abbr -a -g tp trash-put
-    abbr -a -g tl trash-list
-    abbr -a -g te trash-empty
-    abbr -a -g tt trash-restore
 end
 
 # vim: ft=fish
